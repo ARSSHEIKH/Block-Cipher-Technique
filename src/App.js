d863550d6fce90da970188d42b7fabd167138eb0
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-10-06 13:18:24 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+      <h1> Block Cipher Technique </h1>
+      <form>
+        <textarea placeholder=" Enter Plain Text"/>
+        <br/>
+        <input type="submit"/>
+        </form> 
+    </div>
+    </div>
+  );
+}
+
+export default App;
